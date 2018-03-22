078bf8e7e477fb546615a104beaf969a97beceb2
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-03-21 20:39:42 -0600

    Initial commit

---
+++ Java/Warmup-1/StartOz.java
@@ -0,0 +1,13 @@
+/*
+Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+
+startOz("ozymandias") → "oz"
+startOz("bzoo") → "z"
+startOz("oxx") → "o"
+ */
+
+public class StartOz {
+    public String startOz(String str) {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### Coding Bat exercises in Java and Python
+
+*Clone into your favorite IDE and get to work*
