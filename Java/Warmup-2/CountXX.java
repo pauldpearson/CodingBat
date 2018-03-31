fc977ea809752abda4bfec5b8c7f9d1f6973c690
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-03-31 11:35:51 -0600

    finished adding all classes for Warmup-2
    Signed-off-by: Paul Pearson <pauldarlingtonpearson@gmail.com>

---
+++ Java/Warmup-2/CountXX.java
@@ -0,0 +1,10 @@
+/*
+Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+countXX("abcxx") → 1
+countXX("xxx") → 2
+countXX("xxxx") → 3
+ */
+
+public class CountXX {
+}
---
+++ Java/Warmup-2/StringBits.java
@@ -0,0 +1,10 @@
+/*
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+stringBits("Hello") → "Hlo"
+stringBits("Hi") → "H"
+stringBits("Heeololeo") → "Hello"
+ */
+
+public class StringBits {
+}
