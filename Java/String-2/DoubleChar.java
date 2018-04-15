1af9783f1c03ac9d342101a1a86a14a6fa04d456
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-04-15 12:51:07 -0600

    added classes for String-2
    Signed-off-by: Paul Pearson <pauldarlingtonpearson@gmail.com>

---
+++ Java/String-2/DoubleChar.java
@@ -0,0 +1,10 @@
+/*
+Given a string, return a string where for every char in the original, there are two chars.
+
+doubleChar("The") → "TThhee"
+doubleChar("AAbb") → "AAAAbbbb"
+doubleChar("Hi-There") → "HHii--TThheerree"
+ */
+
+public class DoubleChar {
+}
---
+++ Java/String-2/StarOut.java
@@ -0,0 +1,10 @@
+/*
+Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+starOut("ab*cd") → "ad"
+starOut("ab**cd") → "ad"
+starOut("sm*eilly") → "silly"
+ */
+
+public class StarOut {
+}
