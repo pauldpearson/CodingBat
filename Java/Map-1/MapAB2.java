44bdb201db38d9ea20ac12772895bf4a76dea5dd
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-04-16 21:02:33 -0600

    added classes for Map-1, and readme
    Signed-off-by: Paul Pearson <pauldarlingtonpearson@gmail.com>

---
+++ Java/Map-1/MapAB2.java
@@ -0,0 +1,10 @@
+/*
+Modify and return the given map as follows: if the keys "a" and "b" are both in the map and have equal values, remove them both.
+
+mapAB2({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
+mapAB2({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
+mapAB2({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
+ */
+
+public class MapAB2 {
+}
---
+++ Java/Map-1/MapAB3.java
@@ -0,0 +1,10 @@
+/*
+Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a value in the map (but not both), set the other to have that same value in the map.
+
+mapAB3({"a": "aaa", "c": "cake"}) → {"a": "aaa", "b": "aaa", "c": "cake"}
+mapAB3({"b": "bbb", "c": "cake"}) → {"a": "bbb", "b": "bbb", "c": "cake"}
+mapAB3({"a": "aaa", "b": "bbb", "c": "cake"}) → {"a": "aaa", "b": "bbb", "c": "cake"}
+ */
+
+public class MapAB3 {
+}
