a5465aaa5aa29183680b4724aa2692e8fa5a2060
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-02-04 15:27:13 -0500

    removing class

--- Java/Warmup-1/Makes10.java
+++ Java/Warmup-1/Makes10.java
@@ -7,4 +7,14 @@ makes10(1, 9) → true
  */
 
 public class Makes10 {
+
+    private static boolean makes10(int a, int b) {
+        return (a == 10 || b == 10 || a + b == 10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makes10(9, 10));
+        System.out.println(makes10(9, 9));
+        System.out.println(makes10(1, 9));
+    }
 }
